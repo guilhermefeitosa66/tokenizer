d4789f3dac4bed89226aa259a3939d3d03c62941
Guilherme <guilhermefeitosa66@gmail.com>
2016-01-24 12:37:45 -0300

    find tokenized words

--- app.rb
+++ app.rb
@@ -4,11 +4,31 @@ require "uri"
 t = Tokenizer::WhitespaceTokenizer.new
 text = ""
 
-# ler o arquivo
+# ler o arquivo de texto
 File.open('text.txt', 'r') do |file|
   while line = file.gets
     text << line
   end
 end
 
-p t.tokenize(text)
+# deixa todas os caracteres minúsculos
+text.downcase!
+# substitui todas as quebras-de-linha por espaços
+text = text.gsub("\n", " ")
+
+# separa todas as palavras em um array
+words = t.tokenize(text)
+
+# imprime o vetor
+p words
+
+# verifica se uma lista de palavras existe no vetor
+words_list = ['hello', 'como', 'blabla']
+words_found = []
+
+words_list.each do |word|
+  words_found << words.include?(word)
+end
+
+# imprime lista do resultado da busca das palavras
+p words_found
